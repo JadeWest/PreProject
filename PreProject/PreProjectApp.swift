e3f1ffe42cc27a6ee0be5571997453ab27d1d9d8
JadeWest <shng93@outlook.com>
2023-03-02 08:53:01 +0900

    Initial Commit

---
+++ PreProject/PreProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreProjectApp.swift
+//  PreProject
+//
+//  Created by 서현규 on 2023/03/02.
+//
+
+import SwiftUI
+
+@main
+struct PreProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
